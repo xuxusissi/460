f3a2ce2b5226f0dfb399c7ec850ef839c0b451cc
TINGW <tingw@bu.edu>
2017-10-29 15:38:44 -0400

    added placeholder

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app.secret_key = 'super secret string'  # Change this!
 
 # These will need to be changed according to your creditionals
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'xuxu'
+app.config['MYSQL_DATABASE_PASSWORD'] = '0000'
 app.config['MYSQL_DATABASE_DB'] = 'photoshare'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
